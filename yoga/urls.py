b3eb5ae29654633ed5bd50cac7640a5ca17c1439
Yaron <prsnca@gmail.com>
2013-12-16 10:59:03 +0200

    register added

--- yoga/urls.py
+++ yoga/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from lessons import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^lessons/', include('lessons.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'lessons/login.html'}),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'lessons/index.html'}),
     (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'lessons/logout.html'}),
 )
