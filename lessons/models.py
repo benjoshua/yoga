532a2dd688ef26ae7026bd132f55416af98c3d1c
Itay Bittan <itay.bittan@gmail.com>
2013-12-21 10:47:38 +0200

    fix StudenClassTime unicode + refactoring Lesson

--- lessons/models.py
+++ lessons/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Lesson(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField('Lesson Time')
+    def current_time(self):
+        return timezone.now()
+    
+    def spots_left(self):
+        return 15
+    
     def __unicode__(self):
         return self.name
 
@@ -11,4 +18,4 @@ class StudentsClassTime(models.Model):
     student = models.ForeignKey(User)
     lesson = models.ForeignKey(Lesson)
     def __unicode__(self):
-        return self.student + ', ' + self.lesson
+        return unicode(self.student) + ', ' + unicode(self.lesson)
