2d04df266bb70c0a98c9870d13a4feac8e4886fd
Itay Bittan <itay.bittan@gmail.com>
2013-12-22 16:08:53 +0200

    Update Model, delete table, add 'signup' & 'remove' buttons

--- lessons/models.py
+++ lessons/models.py
@@ -5,17 +5,20 @@ from django.utils import timezone
 class Lesson(models.Model):
     name = models.CharField(max_length=200)
     date = models.DateTimeField('Lesson Time')
+    students = models.ManyToManyField(User, null=True, blank=True)
+
+    def student_list(self):
+        return ", ".join([student.first_name for student in self.students.all()])
+    student_list.short_description = "Students"
+    
+    def student_array(self):
+        return self.students.all()
+    
     def current_time(self):
         return timezone.now()
     
     def spots_left(self):
-        return 15
+        return 15 - self.students.count()
     
     def __unicode__(self):
         return self.name
-
-class StudentsClassTime(models.Model):
-    student = models.ForeignKey(User)
-    lesson = models.ForeignKey(Lesson)
-    def __unicode__(self):
-        return unicode(self.student) + ', ' + unicode(self.lesson)
