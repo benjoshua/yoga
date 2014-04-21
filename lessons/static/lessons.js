69716d1b4e702054f8f18dee176888ae8ab3217c
Yaron <prsnca@gmail.com>
2014-04-21 13:09:40 +0300

    added date

--- lessons/static/lessons.js
+++ lessons/static/lessons.js
@@ -5,23 +5,24 @@ lessonsApp.config(function ($interpolateProvider) {
     $interpolateProvider.endSymbol('}]}');
 });
 
-lessonsApp.config(['$httpProvider', function($httpProvider) {
-    $httpProvider.defaults.headers.common['X-CSRFToken'] = '{{ csrf_token|escapejs }}';
-}]);
-
 lessonsApp.controller('LessonsCtrl', ['$http', '$scope', function ($http, $scope) {
 
     $scope.lessons = [];
 
-    $http.get('/lessons/rest_api/')
-        .success(function (data) {
-            $scope.lessons = data;
-        });
+    $scope.getLessons = function () {
+        $http.get('/lessons/rest_api/')
+            .success(function (data) {
+                $scope.lessons = data;
+            });
+    }
+
+    $scope.getLessons();
 
     $scope.signup = function (lesson_id) {
         $http.post('/lessons/rest_api/signup/', {lesson: lesson_id})
             .success(function () {
                 alert("signed up successfully");
+                $scope.getLessons();
             })
             .error(function (data) {
                 alert("error signing up: " + data);
@@ -33,6 +34,7 @@ lessonsApp.controller('LessonsCtrl', ['$http', '$scope', function ($http, $scope
         $http.post('/lessons/rest_api/remove/', {lesson: lesson_id})
             .success(function () {
                 alert("removed successfully from class");
+                $scope.getLessons();
             })
             .error(function (data) {
                 alert("error removing: " + data);
